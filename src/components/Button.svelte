e4fbb489303222be0dd30dc887fa80c04b9e7eda
Sherif Elmetwally <sge5@njit.edu>
2024-02-19 22:19:36 -0500

    update padding

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   export let type: "button" | "submit";
+  export let onClick: () => void;
 </script>
 
 <button 
-  class="border border-raison color-black bg-gray-100 hover:bg-gray-200 py-2 font-semibold px-3 w-full text-raison" 
+  class="border border-raison color-black bg-gray-100 hover:bg-gray-200 py-1 font-semibold px-3 w-full text-raison" 
   type={type}
+  on:click={onClick}
 >
   <slot />
 </button>
