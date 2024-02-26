2571b94d9ad267767fa0d72db240c114348b9b69
Sherif Elmetwally <sge5@njit.edu>
2024-02-25 21:12:00 -0500

    add some themes

--- src/routes/story/[storyId]/+page.svelte
+++ src/routes/story/[storyId]/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	import Sentence from './components/Sentence.svelte';
 	import type { PageData } from './$types';
 	import { Mode } from './types';
 	import Header from './components/Header.svelte';
-  import Drawer from '../../../components/Drawer.svelte';
+	import Drawer from '../../../components/Drawer.svelte';
 	export let data: PageData;
 	const sentences = data.formattedStory?.text || [];
 	const sentencesLength = sentences.length || 0;
@@ -14,35 +16,45 @@
 	let showTransliterated = true;
 	let showWordDetails = true;
 
-  let mode = Mode.SingleText
+	let mode = Mode.SingleText;
 
-  let isOpen = true;
+	let isOpen = true;
 
-  function updateMode(event: Event) {
-    const value = (event.target as HTMLInputElement).value;
+	let root, doc;
 
-    switch (value) {
-      case 'single-text':
-        mode = Mode.SingleText;
-        break;
-      case 'bi-text':
-        mode = Mode.BiText;
-        break;
-      case 'sentence-view':
-        mode = Mode.SentanceView;
-        break;
-      default:
-        break;
-    }
-  }
+	onMount(() => {
+		// Access document here
+		root = document.documentElement;
+		doc = document.firstElementChild;
 
-  function handleOpenDrawer() {
-    isOpen = true;
-  }
+		// Your code using root and doc here
+	});
 
-  function handleCloseDrawer() {
-    isOpen = false;
-  }
+	function updateMode(event: Event) {
+		const value = (event.target as HTMLInputElement).value;
+
+		switch (value) {
+			case 'single-text':
+				mode = Mode.SingleText;
+				break;
+			case 'bi-text':
+				mode = Mode.BiText;
+				break;
+			case 'sentence-view':
+				mode = Mode.SentanceView;
+				break;
+			default:
+				break;
+		}
+	}
+
+	function handleOpenDrawer() {
+		isOpen = true;
+	}
+
+	function handleCloseDrawer() {
+		isOpen = false;
+	}
 
 	function nextSentence() {
 		if (sentence === sentencesLength - 1) {
@@ -81,37 +93,108 @@
 	// 	}
 	// }
 
-  const modeOptions = [
-    { value: 'single-text', text: 'Single Text'},
-    { value: 'bi-text', text: 'Bi Text'},
-    { value: 'sentence-view', text: 'Sentence View' },
-  ];
+	const modeOptions = [
+		{ value: 'single-text', text: 'Single Text' },
+		{ value: 'bi-text', text: 'Bi Text' },
+		{ value: 'sentence-view', text: 'Sentence View' }
+	];
+
+	const onHue = (e) => {
+		localStorage.setItem('--brand-hue', e.target.value);
+		root.style.setProperty('--brand-hue', e.target.value);
+	};
+
+	const onTheme = (e) => {
+		const val = e.target.value;
+		console.log({ val });
+		localStorage.setItem('color-scheme', e.target.value);
+    doc.setAttribute('color-scheme', e.target.value)
+	};
 </script>
 
 <main class="h-full p-3">
-  <fieldset>
-    <legend>Select Mode</legend>
-    <ul class="flex flex-col gap-2">
-      {#each modeOptions as option}
-        <li>
-          <label
-            for={option.value}
-            class="flex gap-4 flex-row items-center border border-raison w-full p-2 text-base sm:cursor-pointer"
-          >
-            <input
-              type="radio"
-              name="mode"
-              id={option.value}
-              value={option.value}
-              on:change={updateMode}
-            />
-            <span class="overflow-hidden text-ellipsis text-base font-normal whitespace-nowrap select-none">{option.text}</span>
-          </label>
-        </li>
-      {/each}
-    </ul>
-  </fieldset>
-  <button on:click={handleOpenDrawer}>open</button>
+	<div class="content input-wrapper">
+		<input
+			on:input={onHue}
+			type="range"
+			value="200"
+			min="0"
+			max="260"
+			class="brand-hue-input"
+			aria-label="hue-slider"
+			tabindex="0"
+			role="slider"
+		/>
+	</div>
+	<form on:input={onTheme}>
+		<div>
+			<input
+				checked
+				type="radio"
+				id="light"
+				name="theme"
+				value="light"
+				class="radio-input"
+				aria-label="radio-button-light-theme"
+				tabindex="0"
+			/>
+		</div>
+		<div>
+			<input
+				type="radio"
+				id="dim"
+				name="theme"
+				value="dim"
+				class="radio-input"
+				aria-label="radio-button-dim-theme"
+				tabindex="0"
+			/>
+		</div>
+		<div>
+			<input
+				type="radio"
+				id="dark"
+				name="theme"
+				value="dark"
+				class="radio-input"
+				aria-label="radio-button-dark-theme"
+				tabindex="0"
+			/>
+		</div>
+	</form>
+
+	<div class="one"></div>
+	<div class="two"></div>
+	<div class="three"></div>
+	<div class="four"></div>
+	<div class="five"></div>
+	<div class="six"></div>
+	<fieldset>
+		<legend>Select Mode</legend>
+		<ul class="flex flex-col gap-2">
+			{#each modeOptions as option}
+				<li>
+					<label
+						for={option.value}
+						class="flex w-full flex-row items-center gap-4 border border-raison p-2 text-base sm:cursor-pointer"
+					>
+						<input
+							type="radio"
+							name="mode"
+							id={option.value}
+							value={option.value}
+							on:change={updateMode}
+						/>
+						<span
+							class="select-none overflow-hidden text-ellipsis whitespace-nowrap text-base font-normal"
+							>{option.text}</span
+						>
+					</label>
+				</li>
+			{/each}
+		</ul>
+	</fieldset>
+	<button on:click={handleOpenDrawer}>open</button>
 
 	<section class="mt-8 flex flex-col divide-y divide-raison">
 		{#each sentences as sentence}
@@ -120,25 +203,7 @@
 	</section>
 </main>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	<!-- <Header
+<!-- <Header
 		key={data.story[0].key}
     {handleOpenDrawer}
 		{previousSentence}
@@ -150,3 +215,36 @@
 		{showWordDetails}
 		{updateBlocksToShow}
 	/> -->
+
+<style>
+	.one {
+		background-color: var(--tile1);
+		height: 100px;
+		width: 100px;
+	}
+	.two {
+		background-color: var(--tile2);
+		height: 100px;
+		width: 100px;
+	}
+	.three {
+		background-color: var(--tile3);
+		height: 100px;
+		width: 100px;
+	}
+	.four {
+		background-color: var(--tile4);
+		height: 100px;
+		width: 100px;
+	}
+	.five {
+		background-color: var(--tile5);
+		height: 100px;
+		width: 100px;
+	}
+	.six {
+		background-color: var(--tile6);
+		height: 100px;
+		width: 100px;
+	}
+</style>
