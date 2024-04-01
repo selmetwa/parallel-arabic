ee8193ad3056f4751ac2787700480e74c0812cb0
Sherif Elmetwally <sge5@njit.edu>
2024-04-01 00:34:05 -0400

    add footer

--- src/routes/write/+page.svelte
+++ src/routes/write/+page.svelte
@@ -5,7 +5,7 @@
 
 
 <p class="text-text-200 text-xl pl-8 mt-12">Practice writing over 4,000 words.</p>
-<section class="grid grid-cols-1 sm:grid-cols-2 gap-3 p-8">
+<section class="grid grid-cols-1 sm:grid-cols-2 gap-3 p-8 mb-12">
   {#each sections as section}
       <a 
       class="p-4 border-2 border-tile-600 text-center bg-tile-400 hover:bg-tile-500 transitional-all duration-300 cursor-pointer font-semibold"
