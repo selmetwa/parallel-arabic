36a0ceb5f3072e876fa44b9f0504d6b6ef9f1e8c
Sherif Elmetwally <sge5@njit.edu>
2024-06-02 18:08:58 -0400

    add about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<section class="text-center mt-14  px-6 sm:px-12 py-8">
+  <h1 class="text-4xl text-text-300 font-semibold">Parallel Arabic</h1>
+  <div class="text-left text-text-200 mt-4">
+    <h2 class="text-xl sm:text-2xl font-medium font-semibold">
+      Parallel Arabic is an application which enables users to read egyptian arabic texts alongside the same text in English.
+    </h2>
+    <p class="mt-2 text-text-200 font-medium text-base sm:text-lg">
+      Each story features a dictionary of key words, transliteration of the text, and full native audio recordings, creating a fully self-contained learning environment without the need for outside resources.
+    </p>
+    <p class="mt-2 text-text-200 font-medium text-base sm:text-lg">
+      There is also a section for writing where you can practice writing over 4000 words, with realtime spellchecking. The tool contains a fully featured Arabic virtual keyboard, built for english speakers.
+    </p>
+  </div>
+</section>
