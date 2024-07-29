9a2f60e1caa46c33fa7fc71edee0bdb8cedfbab5
Sherif Elmetwally <sge5@njit.edu>
2024-07-28 23:23:31 -0400

    add close button to modal

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <button 
-  class={cn("border border-tile-600 text-text-300 bg-tile-500 hover:bg-tile-600 py-1 font-semibold px-3 w-full whitespace-nowrap text-text-300", className)}
+  class={cn("border border-tile-600 text-text-300 bg-tile-500 hover:bg-tile-600 duration-300 py-1 font-semibold px-3 w-full whitespace-nowrap text-text-300", className)}
   type={type}
   on:click={onClick}
 >
