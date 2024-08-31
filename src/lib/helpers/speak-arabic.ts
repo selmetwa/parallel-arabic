e7ee77ce8c8054e118e6a6c31dec3ccb289f00c6
Sherif Elmetwally <sge5@njit.edu>
2024-09-01 00:43:44 +0300

    some grid cleanup

--- src/lib/helpers/speak-arabic.ts
+++ src/lib/helpers/speak-arabic.ts
@@ -1,24 +1,4 @@
 import EasySpeech from 'easy-speech';
-
-// export const speakArabic = async (text: string) => {
-//   const res = await fetch('/api/text-to-speech', {
-//     method: 'POST',
-//     headers: { accept: 'audio/mpeg' },
-//     body: JSON.stringify({
-//       text: text
-//     })
-//   });
-
-//   const blob = await res.blob();
-
-//   const audioUrl = URL.createObjectURL(blob);
-
-//   // Optionally, play the audio
-//   const audio = new Audio(audioUrl);
-//   audio.play();
-//   // return blob;
-// }
-
 export const speakArabic = async (text: string) => {
 	const a = EasySpeech.detect();
 
