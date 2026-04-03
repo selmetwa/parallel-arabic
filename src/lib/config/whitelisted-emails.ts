ee12d81e7fd6a3b21961f035e684979464c13d41
Sherif Elmetwally <sherifelmetwally@Sherifs-MacBook-Pro.local>
2026-04-03 17:22:37 -0400

    Asdasd

--- src/lib/config/whitelisted-emails.ts
+++ src/lib/config/whitelisted-emails.ts
@@ -2,5 +2,6 @@ export const WHITELISTED_EMAILS = [
   'anguszeng.az@gmail.com',
   'jhs@allclear.co.uk',
   'sherifliketheclash+5@gmail.com',
-  "totallynew@gmail.com"
+  "totallynew@gmail.com",
+  'ceciferreri18@gmail.com'
 ];
